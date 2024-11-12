643452a6869733bfccfe16d8e51e94a398e07f4e
mirac <miraclegamor@gmail.com>
2024-11-12 14:24:17 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('turnOn').onclick = function() {
+    document.getElementById('myImage').src = 'pic_bulbon.jpeg';
+};
+
+document.getElementById('turnOff').onclick = function() {
+    document.getElementById('myImage').src = 'pic_bulboff.jpeg';
+};
